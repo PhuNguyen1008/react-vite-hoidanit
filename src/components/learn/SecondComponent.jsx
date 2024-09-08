c55875398eaf4df6c3946a8b99ad7d3799cf0b3a
NguyenDaiPhu <nguyendaiphubkdn@gmail.com>
2024-09-09 00:29:01 +0900

    video 29 import/export component

---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,18 @@
+
+const ThirdComponent = () => {
+    return (
+        <div>
+            Third Component
+        </div>
+    );
+}
+
+const SecondComponent = () => {
+    return (
+        <div>
+            Second Component
+        </div>
+    );
+}
+
+export { SecondComponent, ThirdComponent };
