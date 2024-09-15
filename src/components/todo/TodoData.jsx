d09864ad5b73258a705b767bc74dee74c40fdf81
NguyenDaiPhu <nguyendaiphubkdn@gmail.com>
2024-09-15 19:01:42 +0900

    video 34 props

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,10 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    //props la 1 object
+    const { name, age, data } = props;
+    console.log(">>>check props: ", props)
     return (
         <div className='todo-data'>
+            <div>My name is {name}</div>
             <div>Learn React</div>
             <div>Watch Youtube</div>
         </div>
