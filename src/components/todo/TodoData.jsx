cd7b01a2c83ba4e1a46610451b1601b191fe6f43
NguyenDaiPhu <nguyendaiphubkdn@gmail.com>
2024-09-20 00:28:07 +0900

    video 42 delete Todo

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -2,7 +2,11 @@ const TodoData = (props) => {
 
     //props la 1 object
     //destructuring object
-    const { todoList } = props;
+    const { todoList, deleteTodo } = props;
+
+    const handleClick = (id) => {
+        deleteTodo(id);
+    }
 
     return (
         <div className='todo-data'>
@@ -10,7 +14,10 @@ const TodoData = (props) => {
                 return (
                     <div className={'todo-item'} key={item.id}>
                         <div>{item.name}</div>
-                        <button>Delete</button>
+                        <button
+                            onClick={() => handleClick(item.id)}
+                            style={{ cursor: "pointer" }}
+                        >Delete</button>
                     </div>
                 )
             })}
