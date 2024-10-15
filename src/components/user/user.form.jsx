a046f41127b5eec4e69605c37be82b0c9bf00358
NguyenDaiPhu <nguyendaiphubkdn@gmail.com>
2024-10-16 01:22:31 +0900

    video 70 Config Axios Interceptor

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,23 +1,21 @@
-import { Button, Input } from "antd";
+import { Button, Descriptions, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
-    const [fullName, setFullName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [phone, setPhone] = useState("")
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
 
-    const handleOnClick = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user"
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
+    const handleOnClick = async () => {
+        const res = await createUserAPI(fullName, email, password, phone)
+        if (res.data) {
+            notification.success({
+                message: "create user",
+                description: "Tạo user thành công"
+            })
         }
-        axios.post(URL_BACKEND, data)
-        console.log(">>>check stage:", { fullName, email, password, phone })
     }
 
     return (
